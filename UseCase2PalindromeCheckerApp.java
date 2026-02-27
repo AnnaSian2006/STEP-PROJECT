a4a60a7d9bf868ea120e542ff5c0c44b61d272a7
AnnaSian2006 <anna.sian2006@gmail.com>
2026-02-27 10:25:45 +0530

    Update UseCase2PalindromeCheckerApp.java

--- UseCase2PalindromeCheckerApp.java
+++ UseCase2PalindromeCheckerApp.java
@@ -2,18 +2,18 @@ public class UseCase2PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Hardcoded string (String Literal)
+        
         String word = "madam";
 
-        // Variable to store reversed string
+        
         String reversed = "";
 
-        // Reverse the string using loop
+        
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
 
-        // Check if original string equals reversed string
+        
         if (word.equals(reversed)) {
             System.out.println("The given string \"" + word + "\" is a Palindrome.");
         } else {
