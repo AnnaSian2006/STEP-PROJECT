1ea2f1165b1205553477454cafd0da25cca8e5c9
AnnaSian2006 <anna.sian2006@gmail.com>
2026-02-27 10:33:42 +0530

    Refactor PalindromeChecker and strategy classes
    Refactor PalindromeChecker to improve strategy implementation.

--- UseCase12PalindromeCheckerApp.java
+++ UseCase12PalindromeCheckerApp.java
@@ -1,10 +1,10 @@
-// Strategy Interface
+
 interface PalindromeStrategy {
     boolean checkPalindrome(String input);
 }
 
 
-// Stack-Based Strategy
+
 class StackStrategy implements PalindromeStrategy {
 
     @Override
@@ -13,12 +13,12 @@ class StackStrategy implements PalindromeStrategy {
         input = input.toLowerCase();
         java.util.Stack<Character> stack = new java.util.Stack<>();
 
-        // Push all characters to stack
+       
         for (char ch : input.toCharArray()) {
             stack.push(ch);
         }
 
-        // Compare with original string
+   
         for (char ch : input.toCharArray()) {
             if (ch != stack.pop()) {
                 return false;
@@ -54,12 +54,12 @@ class DequeStrategy implements PalindromeStrategy {
 }
 
 
-// Context Class
+
 class PalindromeContext {
 
     private PalindromeStrategy strategy;
 
-    // Constructor Injection
+    
     public PalindromeContext(PalindromeStrategy strategy) {
         this.strategy = strategy;
     }
@@ -74,16 +74,16 @@ class PalindromeContext {
 }
 
 
-// Main Application
+
 public class UseCase12PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String input = "level";
 
-        // Choose strategy dynamically
+        
         PalindromeStrategy strategy = new StackStrategy();
-        // Try changing to: new DequeStrategy();
+        
 
         PalindromeContext context = new PalindromeContext(strategy);
 
