044ce181ac720965177efa085fa8c4f02978d162
AnnaSian2006 <anna.sian2006@gmail.com>
2026-02-27 08:44:50 +0530

    uc2

---
+++ UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string (String Literal)
+        String word = "madam";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string using loop
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check if original string equals reversed string
+        if (word.equals(reversed)) {
+            System.out.println("The given string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
+        }
+    }
+}
